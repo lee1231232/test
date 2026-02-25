98e3cb3536b34109df86550a42969a0598f649c4
dlqhdrn777 <dlqhdrn777@gmail.com>
2026-02-25 01:47:32 +0000

    Update Bong-gu Lee to Lee in Terms of Service

--- main.js
+++ main.js
@@ -232,13 +232,13 @@ function renderTerms() {
             <p>By accessing this website, you are agreeing to be bound by these website Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws.</p>
             
             <h3>1. Use License</h3>
-            <p>Permission is granted to temporarily download one copy of the materials (information or software) on Bong-gu Lee's Minimal Blog for personal, non-commercial transitory viewing only.</p>
+            <p>Permission is granted to temporarily download one copy of the materials (information or software) on Lee's Minimal Blog for personal, non-commercial transitory viewing only.</p>
             
             <h3>2. Disclaimer</h3>
             <p>The materials on this website are provided "as is". We make no warranties, expressed or implied, and hereby disclaim and negate all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
             
             <h3>3. Limitations</h3>
-            <p>In no event shall Bong-gu Lee or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on the blog.</p>
+            <p>In no event shall Lee or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on the blog.</p>
         </article>
     `;
 }
